b4a6f3dc2f3c01bf7abed169663b860d78a75ecb
gustojs <gustojs@protonmail.com>
2021-10-23 23:47:47 +0200

    initial commit

---
+++ app.vue
@@ -0,0 +1,18 @@
+<template>
+  <p>{{ counter }}</p>
+  <button @click="onClick1">click me</button>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+const counter = ref(0);
+
+function onClick1() {
+  console.log("onClick1");
+  counter.value++;
+}
+function onClick2() {
+  console.log("onClick2");
+  counter.value++;
+}
+</script>
